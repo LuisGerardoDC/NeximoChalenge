caa9ed0291b9f601a71bbc087f30bb38e470b318
luis <luisgerarhern@outlook.com>
2023-06-29 10:05:36 -0600

    endpoints added

--- NeximoChallenge/myapp/views.py
+++ NeximoChallenge/myapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def registerEndpoint(request):
+    if request.method == 'POST':
+        pass
+    else:
+        pass
+
+def paymentsEnpoint(request):
+    if request.method == 'POST':
+        pass
+    else:
+        pass
+
+def loginEndpoint(request):
+    if request.method == 'POST':
+        pass
+    else:
+        pass
